6bec7ed692e2fdc671de5aca8b90a4e7d6eb6282
lahirujay99 <lahirujayawardana94@gmail.com>
2023-05-31 22:23:55 +0530

    before test

---
+++ app/src/main/java/com/lahiru/roomdb2/MainActivity.kt
@@ -0,0 +1,78 @@
+ package com.lahiru.roomdb2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings.Global
+import android.widget.Toast
+import com.lahiru.roomdb2.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+ class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var appDb : AppDatabase
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnWriteData.setOnClickListener {
+            writeData()
+        }
+
+        binding.btnReadData.setOnClickListener {
+            readData()
+        }
+
+
+    }
+
+     private fun writeData(){
+         val firstName = binding.etFirstName.text.toString()
+         val lastName = binding.etLastName.text.toString()
+         val rollNo = binding.etRollNo.text.toString()
+
+
+         if(firstName.isNotEmpty() && lastName.isNotEmpty() && rollNo.isNotEmpty()){
+             val student = Student(
+                 null,firstName,lastName,rollNo.toInt()
+             )
+             GlobalScope.launch(Dispatchers.IO){
+                 appDb.studentDao().insert(student)
+             }
+             binding.etFirstName.text.clear()
+             binding.etLastName.text.clear()
+             binding.etRollNo.text.clear()
+             Toast.makeText(this@MainActivity,"Successfully written",Toast.LENGTH_SHORT).show()
+         }else{
+             Toast.makeText(this@MainActivity,"Fail written",Toast.LENGTH_SHORT).show()
+
+         }
+
+     }
+
+
+     private suspend fun displayData(student: Student){
+         withContext(Dispatchers.Main){
+             binding.tvFirstName.text=  student.firstName
+             binding.tvLastName.text=  student.lastName
+             binding.tvRollNo.text=  student.rollNo.toString()
+
+         }
+     }
+
+     private fun readData(){
+         val rollNo = binding.etRollNoRead.text.toString()
+
+         if(rollNo.isNotEmpty()){
+             lateinit var student: Student
+
+             GlobalScope.launch {
+                 student = appDb.studentDao().findByRoll(rollNo.toInt())
+                 displayData(student)
+             }
+         }
+     }
+}
